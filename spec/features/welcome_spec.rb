a9acd599fd0178d639cf52da77d3dcb13a0b8212
EmilyPlatzer <emilyplatzer@gmail.com>
2015-02-01 15:41:00 -0700

    User is welcomed on homepage
    Added Bootstrap, basic outline of homepage

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'Welcome page - ' do
+  scenario 'User is greeted on homepage' do
+    visit '/'
+    within '.container' do
+      within '.jumbotron' do
+        expect(page).to have_content('Emily and Luke')
+        expect(page).to have_content('May 16th, 2015')
+        expect(page).to have_content('Lyons, Colorado')
+      end
+    end
+    within '.nav' do
+      expect(page).to have_link('Our Wedding')
+      expect(page).to have_link('Photos')
+      expect(page).to have_link('RSVP')
+      expect(page).to have_link('Registry')
+    end
+  end
+end
